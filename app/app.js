999aef0bb11ae6fe3a4fa893976665cc3f56b9bf
hoang.tuan.anh <hoang.tuan.anh@framgias-Mac-mini.local>
2018-06-07 10:20:28 +0700

    initial commit

--- app/app.js
+++ app/app.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import 'bootstrap/dist/css/bootstrap.css'
 
 import AppRoutes from './routes'
-import store from './store'
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
     <AppRoutes />
   </Provider>,
   document.getElementById('app')
-)
+);
